d3da1163c7ad7086351a3832baf82cf88a23f3de
Victor Safronovich <vsafronovich@gmail.com>
2013-06-02 00:42:03 +0600

    some working methods

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 python-pechkin
 ==============
 
-integration with pechkin-mail.ru
+Интеграция pechkin-mail.ru и вашего приложения
 
 pechkin API: http://pechkin-mail.ru/?page=api
+
+Пример кода
+
+    from pechkin.api import PechkinApi
+
+    api = PeckinApi('my_username', 'my_password')
+
+    api.lists_get()  # Показывает все ваши базы
+
+    list_id = api.lists_add('Первая База')
+
+    plist = api.lists_get(list_id)
+
+    plist.add_member(email='vsafronovich@gmail.com')
 
--- pechkin/api.py
+++ pechkin/api.py
@@ -1,4 +1,4 @@
-
+# -*- coding: utf-8 -*-
 import logging
 from functools import partial
 
@@ -21,106 +21,141 @@ class PechkinFixApi(object):
 
 class PechkinApi(object):
 
-    API_URL = 'api.pechkin-mail.ru'
+    # TODO ssl from pechkin bad
+    API_URL = 'http://api.pechkin-mail.ru'
 
     def __init__(self, username, password):
         self._client = requests.Session()
-        self._client.auth = (username, password)
-        self._client.params = {'format': 'json'}
+        #self._client.auth = (username, password)
+        self._client.params = {'format': 'json',
+                               'username': username,
+                               'password': password}
 
     def log_errors(self, response):
-        json = response.json()
+        json = response.json()['response']
         if 'msg' in json:
-            messages = json['msg']
-            for message in messages:
-                if message['type'] == 'message']:
-                    log = logger.info
-                elif message['type'] == 'notice']:
-                    log = logger.warning
-                elif message['type'] == 'error']
-                    log = logger.error
-                else:
-                    log = logger.debug
+            message = json['msg']
 
-                log(message['err_code'] + ': ' + message['text'])
+            if message['type'] == 'message':
+                log = logger.info
+            elif message['type'] == 'notice':
+                log = logger.warning
+            elif message['type'] == 'error':
+                log = logger.error
+            else:
+                log = logger.debug
+
+            log(u'%(err_code)s: %(text)s' % message)
 
     def _api_request(self, **kwargs):
-        response = self._client.get(params=kwargs)
+        response = self._client.get(self.API_URL, params=kwargs)
         self.log_errors(response)
-        return response.json()['data']
+        return response.json()['response']
 
-    def lists_get(self, list_id=None):
-        params = dict(item for item in locals() if item[1] is not None)
+    def _get_params(self, params):
+        return dict(item for item in params.items()
+                    if item[1] is not None and item[0] != 'self')
+
+    def lists_get(self, list_id=None, raw=False):
+        params = self._get_params(locals())
         params['method'] = 'lists.get'
-        return self._api_request(**params)
+        lists = self._api_request(**params)['data']
 
-    def lists_add(self, name, abuse_email=None, abuse_name=None, company=None,
-                  address=None, city=None, zip=None, country=None, url=None, phone=None):
-        params = dict(item for item in locals() if item[1] is not None)
+        if not raw:
+            lists = [PechkinList(self, list['id'], list) for list in lists]
+
+        return lists[0] if list_id else lists
+
+    def lists_add(self, name, abuse_email=None, abuse_name=None,
+                  company=None,
+                  address=None, city=None, zip=None, country=None,
+                  url=None, phone=None):
+        params = self._get_params(locals())
         params['method'] = 'lists.add'
-        return self._api_request(**params)['list_id']
+        return self._api_request(**params)['data']['list_id']
 
-    def lists_update(self, list_id, abuse_email=None, abuse_name=None, company=None, 
-                     address=None, city=None, zip=None, country=None, url=None, phone=None):
-        params = dict(item for item in locals() if item[1] is not None)
+    def lists_update(self, list_id, abuse_email=None, abuse_name=None,
+                     company=None,
+                     address=None, city=None, zip=None, country=None,
+                     url=None, phone=None):
+        params = self._get_params(locals())
         params['method'] = 'lists.update'
         return self._api_request(**params)['msg'][0]['err_code'] == 0
 
     def lists_delete(self, list_id):
-        params = dict(item for item in locals() if item[1] is not None)
+        params = self._get_params(locals())
         params['method'] = 'lists.delete'
-        return self._api_request(**params)['deleted_members']
+        return self._api_request(**params)['data']['deleted_members']
 
-    def lists_get_members(self, list_id, state=None, start=0, limit=None, order='id desc',
-                          member_id=None, email=None):
+    def lists_get_members(self, list_id, state=None, start=0, limit=None,
+                          order='id desc', member_id=None, email=None):
 
         assert state in [None, 'active', 'unsubscribed', 'bounced']
-        params = dict(item for item in locals() if item[1] is not None)
+        params = self._get_params(locals())
         params['method'] = 'lists.get_members'
-        return self._api_request(**params)
+        return self._api_request(**params)['data']
 
-    def lists_upload(self, list_id, file):
-        params = dict(item for item in locals() if item[1] is not None)
+    def lists_upload(self, list_id, file, email, type,
+                     merge_1=None, merge_2=None, merge_3=None,
+                     merge_4=None, merge_5=None,
+                     update=None, sheet_index=None, sheet_name=None):
+        params = self._get_params(locals())
         params['method'] = 'lists.upload'
-        return self._api_request(**params)
+        return self._api_request(**params)['data']
 
     def lists_add_member(self, list_id, email,
-                         merge_1=None, merge_2=None, merge_3=None, merge_4=None, merge_5=None,
+                         merge_1=None, merge_2=None, merge_3=None,
+                         merge_4=None, merge_5=None,
                          update=None):
-        params = dict(item for item in locals() if item[1] is not None)
+        params = self._get_params(locals())
         params['method'] = 'lists.add_member'
-        return self._api_request(**params)['member_id']
+        return self._api_request(**params)['data']['member_id']
 
     def lists_update_member(self, member_id,
-                            merge_1=None, merge_2=None, merge_3=None, merge_4=None, merge_5=None):
-        params = dict(item for item in locals() if item[1] is not None)
+                            merge_1=None, merge_2=None, merge_3=None,
+                            merge_4=None, merge_5=None):
+        params = self._get_params(locals())
         params['method'] = 'lists.update_member'
-        return self._api_request(**params)['msg'][0]['err_code'] == 0
+        return self._api_request(**params)['msg']['err_code'] == 0
 
     def lists_delete_member(self, member_id):
-        params = dict(item for item in locals() if item[1] is not None)
+        params = self._get_params(locals())
         params['method'] = 'lists.delete_member'
-        return self._api_request(**params)['msg'][0]['err_code'] == 0
+        return self._api_request(**params)['msg']['err_code'] == 0
 
-    def lists_unsubscribe_member(self, member_id, reason, email=None, list_id=None):
-        params = dict(item for item in locals() if item[1] is not None)
+    def lists_unsubscribe_member(self, member_id, reason, email=None,
+                                 list_id=None):
+        params = self._get_params(locals())
         params['method'] = 'lists.unsubscribe_member'
         return self._api_request(**params)
 
-    def lists_move_member(self, list_id):
-        raise NotImplementedError
+    def lists_move_member(self, member_id, list_id):
+        params = self._get_params(locals())
+        params['method'] = 'lists.move_member'
+        return self._api_request(**params)['data']
 
-    def lists_copy_member(self, list_id):
-        raise NotImplementedError
+    def lists_copy_member(self, member_id, list_id):
+        params = self._get_params(locals())
+        params['method'] = 'lists.copy_member'
+        return self._api_request(**params)['data']
 
-    def lists_add_merge(self, list_id):
-        raise NotImplementedError
+    def lists_add_merge(self, list_id, type, choices=None,
+                        title=None, req=None, var=None):
+        assert type in ['text', 'date', 'choice']
+        params = self._get_params(locals())
+        params['method'] = 'lists.add_merge'
+        return self._api_request(**params)['msg']['err_code'] == 0
 
-    def lists_update_merge(self, list_id):
-        raise NotImplementedError
+    def lists_update_merge(self, list_id, merge_id, choices=None,
+                           title=None, req=None, var=None):
+        params = self._get_params(locals())
+        params['method'] = 'lists.update_merge'
+        return self._api_request(**params)['data']
 
-    def lists_delete_merge(self, list_id):
-        raise NotImplementedError
+    def lists_delete_merge(self, list_id, merge_id):
+        params = self._get_params(locals())
+        params['method'] = 'lists.delete_merge'
+        return self._api_request(**params)['data']
 
     """ TODO another
 campaigns.get
@@ -142,12 +177,24 @@ reports.geo
 
 
 class PechkinList(object):
-    def __init__(self, api, id):
+    def __init__(self, api, id, data=None):
         self.id = id
+        self.data = data
 
-        for method in 'get', 'add', 'update', 'delete':
+        for method in ['get', 'update', 'delete',
+                       'get_members', 'add_member',
+                       'add_merge', 'update_merge', 'delete_merge']:
             api_method = getattr(api, 'lists_%s' % method)
             setattr(self, method, partial(api_method, list_id=self.id))
+
+        if data is None:
+            self.data = self.get(raw=True)
+
+    def __getattr__(self, name):
+        try:
+            return self.data[name]
+        except KeyError:
+            raise AttributeError(name)
 
 
 class PechkinMember(object):
@@ -173,6 +220,8 @@ class PechkinReport(object):
     def __init__(self, api, id):
         self.id = id
 
-        for method in 'send', 'delivered', 'opened':
+        for method in ['send', 'delivered', 'opened', 'unsubscribed', 'bounced',
+                       'clickstat', 'bouncestat', 'summary',
+                       'clients', 'geo']:
             api_method = getattr(api, 'reports_%s' % method)
             setattr(self, method, api_method)
